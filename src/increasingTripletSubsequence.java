68edffa7e9b89193223bb08c5c336312b8437915
Nehak17 <nkharkar@mail.usf.edu>
2019-04-15 03:03:37 -0400

    commit

---
+++ src/increasingTripletSubsequence.java
@@ -0,0 +1,22 @@
+
+class increasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int firstmin = Integer.MAX_VALUE;
+        int secondmin = Integer.MAX_VALUE;
+        for(int n : nums){
+            if(n<=firstmin){
+                firstmin = n;
+            }
+            else if(n<=secondmin){
+                secondmin =  n;
+            }
+            else{
+                return true;
+            }
+        
+        }
+        
+        
+        return false;
+    }
+}
