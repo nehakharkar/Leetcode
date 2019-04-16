ed125f08f9c84d9a8d71b02a9fe41a77c9069cec
Nehak17 <nkharkar@mail.usf.edu>
2019-04-16 04:04:42 -0400

    commit

--- src/invertBinaryTree.java
+++ src/invertBinaryTree.java
@@ -1,13 +1,11 @@
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+ 
 class invertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
         if( root == null)
