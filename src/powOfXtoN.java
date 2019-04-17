09194d13d140fff57076480395014cb148631f90
nehakharkar <neha.kharkar62@gmail.com>
2019-04-17 04:06:35 -0400

    commit

---
+++ src/powOfXtoN.java
@@ -0,0 +1,13 @@
+
+class powOfXtoN {
+    public double myPow(double x, long n) {
+        if( n == 0)
+            return 1;
+        if(n<0)
+        {
+            n= -n;
+            x = 1/x;
+        }
+        return n%2 == 0 ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+    }
+}
