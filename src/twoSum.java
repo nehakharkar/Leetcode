54604636901a9821d198abf4712f5fb0d8c7a28a
nehakharkar <neha.kharkar62@gmail.com>
2019-04-19 14:30:52 -0400

    commit

---
+++ src/twoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    	Map<Integer,Integer> mp = new HashMap();
+    	int[] res = new int[2];
+    	for(int i = 0; i<nums.length ; i++) {
+        	if(mp.containsKey(target-nums[i])) 
+        	{
+        		res[0]= i;
+        		res[1] = mp.get(target-nums[i]);
+        	}
+        	else {
+        		mp.put(nums[i],i);
+        	}
+
+    	}
+		return res;
+    	
+    }
+
+}
