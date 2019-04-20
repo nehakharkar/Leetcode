ea2420f09d17ec4643d925304117db1bc83062d4
nehakharkar <neha.kharkar62@gmail.com>
2019-04-20 15:43:54 -0400

    commit

---
+++ src/moveZeroes.java
@@ -0,0 +1,18 @@
+
+class moveZeroes {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int i = 0; i< nums.length ; i++){
+            if(nums[i]!= 0)
+            {
+                swap(nums,i,index);
+                index++;
+            }
+        }  
+    }
+    public void swap(int[] arr , int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
