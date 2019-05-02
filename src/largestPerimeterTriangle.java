6bb6f76a4da9fea3c4a51ec9514708c5b3dc99ae
nehakharkar <neha.kharkar62@gmail.com>
2019-05-01 23:54:59 -0400

    commit

--- src/largestPerimeterTriangle.java
+++ src/largestPerimeterTriangle.java
@@ -1,26 +1,20 @@
 import java.util.Arrays;
-
 public class largestPerimeterTriangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] s = {3,2,3,4};
+		int[] s = { 3, 2, 3, 4 };
 		System.out.println(largest(s));
 	}
 
+	static int largest(int[] s) {
+		Arrays.sort(s);
+		int sum = 0;
+		for (int i = s.length - 1; i > s.length - 4; i--) {
+			System.out.println(sum);
+			sum += s[i];
 
-	static int largest(int[] s) 
-	{
-	Arrays.sort(s);
-	int sum = 0;
-	for(int i = s.length-1 ;i>s.length-4;i--) {
-		System.out.println(sum);
-		sum += s[i];
-		
-	}
-
-	
-	
-	return sum;
+		}
+		return sum;
 	}
 }
