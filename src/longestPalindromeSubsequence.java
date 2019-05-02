926915982e3eccefd240cf43191c77ee53c383d2
nehakharkar <neha.kharkar62@gmail.com>
2019-05-01 23:55:39 -0400

    commit

--- src/longestPalindromeSubsequence.java
+++ src/longestPalindromeSubsequence.java
@@ -4,42 +4,38 @@ import java.lang.*;
 import java.io.*;
 
 public class longestPalindromeSubsequence {
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int no = sc.nextInt();
-		for(int i = 0 ; i <no ; i++) 
-		{
+		for (int i = 0; i < no; i++) {
 			String str = sc.next();
 			int n = str.length();
-			System.out.println(LPS(str.toCharArray(), 0,n-1));
+			System.out.println(LPS(str.toCharArray(), 0, n - 1));
 		}
 	}
-	public static int LPS(char str[] , int i , int j) {
-		
-		if(i == j)
-		{
+
+	public static int LPS(char str[], int i, int j) {
+
+		if (i == j) {
 			return 1;
-			
+
 		}
-		if(str[i] == str[j] && i + 1 == j)
-		{
+		if (str[i] == str[j] && i + 1 == j) {
 			return 2;
 		}
-		if(str[i] == str[j]) 
-		{
-			return LPS(str ,i+1, j-1) + 2;
+		if (str[i] == str[j]) {
+			return LPS(str, i + 1, j - 1) + 2;
 		}
-		if(str[i] != str[j]) 
-		{
-			return max(LPS(str ,i, j-1) , LPS(str,i-1,j));
+		if (str[i] != str[j]) {
+			return max(LPS(str, i, j - 1), LPS(str, i - 1, j));
 		}
 		return 0;
-		
-		
+
 	}
-	public static int max(int x , int y) {
-		return x>y ? x : y; 
+
+	public static int max(int x, int y) {
+		return x > y ? x : y;
 	}
-	
+
 }
