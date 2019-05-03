b69165690b51b1f94ef260372ee82dce9bd54181
nehakharkar <neha.kharkar62@gmail.com>
2019-05-02 23:03:42 -0400

    commit

--- src/invertBinaryTree.java
+++ src/invertBinaryTree.java
@@ -1,14 +1,12 @@
-
-
 class invertBinaryTree {
-    public TreeNode invertTree(TreeNode root) {
-        if( root == null)
-            return null;
-        TreeNode right =  invertTree(root.right); 
-        TreeNode left =  invertTree(root.left);
-        root.left = right;
-        root.right = left;
-        return root;
-        
-    }
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null)
+			return null;
+		TreeNode right = invertTree(root.right);
+		TreeNode left = invertTree(root.left);
+		root.left = right;
+		root.right = left;
+		return root;
+
+	}
 }
