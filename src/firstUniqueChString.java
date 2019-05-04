02d35f8909f75edbef8ffe5f91dd13e32f35a074
nehakharkar <neha.kharkar62@gmail.com>
2019-05-03 20:11:35 -0400

    commit

--- src/firstUniqueChString.java
+++ src/firstUniqueChString.java
@@ -22,9 +22,7 @@ public class firstUniqueChString {
         	else {
         		hm.put(c, 1);	
         	}
-        }
-        StringBuilder sb = new StringBuilder();
-	
+        }	
 		for(int i = 0 ;i < ch.length ; i ++) {
 			if(hm.get(ch[i]) == 1) {
 				return i;
